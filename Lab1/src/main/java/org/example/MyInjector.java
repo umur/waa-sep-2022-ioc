d8b986fead18ccf35145699c142e470da70ad980
B.Orgilbat <zettry1@gmail.com>
2022-09-05 14:35:28 -0500

    added my injector

--- Lab1/src/main/java/org/example/MyInjector.java
+++ Lab1/src/main/java/org/example/MyInjector.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.reflections.Reflections;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
 public class MyInjector {
 
+    MyInjector() {
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .forPackage("org.example")
+                .filterInputsBy(new FilterBuilder()
+                        .includePackage("org.example")));
+
+        System.out.println(reflections.getTypesAnnotatedWith(MyBean.class));
+    }
 //    map
 
 //    seach through package
