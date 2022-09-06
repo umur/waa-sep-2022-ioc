9344330916b83d0ebb18efe09b0c39266fa3b034
Tamir Bold <boldtamir@gmail.com>
2022-09-06 14:24:20 -0500

    Implemented

---
+++ lab1/src/main/java/WAA/Lab1Application.java
@@ -0,0 +1,21 @@
+package WAA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Application.class, args);
+        MyInjector inject = new MyInjector();
+        inject.searchObjects();
+
+        try{
+            inject.getBean(AClass.class);
+            inject.getBean(BClass.class);
+        } catch (BeanNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
