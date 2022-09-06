e10583111ee09abb07a9876a46acf434a0995c44
B.Orgilbat <zettry1@gmail.com>
2022-09-06 13:33:54 -0500

    fix: lab 1 finish

--- Lab1/src/main/java/org/example/MyAutowired.java
+++ Lab1/src/main/java/org/example/MyAutowired.java
@@ -5,8 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
-@Target({ElementType.TYPE, ElementType.CONSTRUCTOR}) // for class
+@Target({ElementType.FIELD}) // for field
 @Retention(RetentionPolicy.RUNTIME)
-public @interface MyBean {
+public @interface MyAutowired {
 }
--- Lab1/src/main/java/org/example/MyInjector.java
+++ Lab1/src/main/java/org/example/MyInjector.java
@@ -4,19 +4,45 @@ import org.reflections.Reflections;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyInjector {
-
-    MyInjector() {
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .forPackage("org.example")
-                .filterInputsBy(new FilterBuilder()
-                        .includePackage("org.example")));
-
-        System.out.println(reflections.getTypesAnnotatedWith(MyBean.class));
+    Map<Class<?>, Object> map = new HashMap<>();
+    public MyInjector() {
+        Reflections reflections = new Reflections("org.example");
+        reflections.getTypesAnnotatedWith(MyBean.class).forEach(aClass -> {
+            try {
+                Object t = aClass.newInstance();
+                Arrays.stream(aClass.getFields()).filter(f -> f.isAnnotationPresent(MyAutowired.class))
+                        .toList()
+                        .forEach(field -> {
+                            Class<?> innerClass = field.getType();
+                            try {
+                                Object innerInstance = innerClass.newInstance();
+                                map.put(innerClass, innerInstance);
+                                field.set(t, innerInstance);
+                            } catch (InstantiationException | IllegalAccessException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                map.put(aClass, t);
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
+    public Object getBean(Class<?> clazz)  {
+        try {
+            return map.get(clazz);
+        } catch (Exception e) {
+            throw new Error("Error");
+        }
+    }
+}
 //    map
 
 //    seach through package
 
 //   find class with annotation
-}
